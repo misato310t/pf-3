984c426368ab31bea452958177401000bdf90d22
mimi <gude_tama_8@yahoo.co.jp>
2021-09-21 17:50:54 +0900

    first-commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var position = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 200){
+                $(function(){
+                    $('.fadein').each(function(i){
+                        $(this).delay(i * 400).queue(function(){
+                            $(this).addClass('active');
+                        });
+                    });
+                });
+            }
+        });
+    });
+});
+
+jQuery(".drawer-icon").on("click", function(e) {
+    e.preventDefault();
+
+    jQuery('.drawer-icon').toggleClass('is-click')
+    jQuery('.drawer-content').toggleClass('is-click')
+    jQuery('.drawer-background').toggleClass('is-click')
+
+    return false;
+  });
+
+  jQuery(window).on("scroll", function() {
+    // トップから1000px以上スクロールしたら
+    if (1000 < jQuery(this).scrollTop()) {
+    // is-showクラスをつける
+    jQuery('.totop').addClass( 'is-show' );
+    } else {
+    // 1000pxを下回ったらis-showクラスを削除
+    jQuery('.totop').removeClass( 'is-show' );
+    }
+    });
+    
+    jQuery('.totop').click(function(){
+    // ページの先頭へ移動する
+    jQuery('html,body').animate({scrollTop:0}, 'fast');
+    });
